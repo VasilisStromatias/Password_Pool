4ad66e1aa5aeb12366f28083af9ef42469d35ec3
vstromatias <stromatiasvasilis@gmail.com>
2023-10-06 15:11:00 +0300

    ...

--- password-pool/src/components/Header/Header.js
+++ password-pool/src/components/Header/Header.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import React from "react";
 
-import logo from "../../assets/+POOL.png"
-import './header.css';
-import { Link } from 'react-router-dom';
+import logo from "../../assets/+POOL.png";
+import test from "../../assets/test.png";
+import "./header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div className='header'>
-      <div className='header-wrapper'>
-      <div className='header-inner'>
-        <div className='header-logo'>
-          <Link to="/"> 
-        <img src={logo} alt='this is a logo' width="400" height="50"></img>
-          
-          </Link>
-        </div>
+    <div className="header">
+      <div className="header-wrapper">
+        <div className="header-inner">
+          <div className="header-logo">
+            <Link to="/">
+              <img
+                src={test}
+                alt="this is a logo"
+                width="400"
+                height="50"
+              ></img>
+            </Link>
+          </div>
 
-        <div className='header-links'>
-          <Link to="/login" className='header-login-link'><span>Login</span>  </Link>
-          <Link to="/register" className='header-register-link'><span>Register</span> </Link>
+          <div className="header-links">
+            <Link to="/login" className="header-login-link">
+              <span>Login</span>{" "}
+            </Link>
+            <Link to="/register" className="header-register-link">
+              <span>Register</span>{" "}
+            </Link>
+          </div>
         </div>
-
       </div>
-      </div>
-      
     </div>
-  )
+  );
 }
 
 export default Header;
